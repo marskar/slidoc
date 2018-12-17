916ceb92a858b154ceb91702712be90916085bf6
Martin Skarzynski <marskar@gmail.com>
2018-12-17 16:55:45 -0500

    add functions to main module and 4 tests

--- tests/test_slidoc.py
+++ tests/test_slidoc.py
@@ -9,17 +9,26 @@ import pytest
 from slidoc import slidoc
 
 
-@pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+@pytest.mark.parametrize('bad_targets', ('s5', 123, 'remarkjs'))
+def test_value_error(bad_targets):
+    with pytest.raises(ValueError,
+                       match=str(incorrect) +
+                       ' is not one of the 3 supported formats.'):
+        make_slides(target=bad_targets)
 
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+@pytest.mark.parametrize('not_files', ('not a file', 'this either', 'just no'))
+def test_no_file(not_files):
+    with pytest.raises(RuntimeError, match='source_file is not a valid path'):
+        make_slides(source_file=not_files)
+
+
+@pytest.mark.parametrize('targets', ('slidy', 'dzslides', 'revealjs'))
+def test_doctype(targets):
+    """The output should specify the doctype as html"""
+    assert "<!DOCTYPE html" in make_slides(target=targets)
+
+@pytest.mark.parametrize('targets', ('slidy', 'dzslides', 'revealjs'))
+def test_end_tags(targets):
+    """Properly formatted html should end with closing tags"""
+    assert make_slides(target=targets).endswith('</body>\n</html>\n')
